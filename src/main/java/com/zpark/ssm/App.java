a3e4f60fa95668a61421e54f99e72092483ba72b
月光落在左手上 <zs1837514436@163.com>
2023-05-06 09:21:49 +0800

    Hello Git master second commit

---
+++ src/main/java/com/zpark/ssm/App.java
@@ -0,0 +1,10 @@
+package com.zpark.ssm;
+
+public class App {
+
+    private static void main(){
+        System.out.println("Hello Git");
+        System.out.println("Hello Git master second commit");
+        System.out.println("Hello Git hotfix first commit");
+    }
+}
